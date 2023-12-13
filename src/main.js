e2a2de01557aff2652b2ec3d78acfc1d3633a53a
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-13 17:38:38 +0100

    feat: import global styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import LoginPage from './views/LoginPage.vue';
 import HomePage from './views/HomePage.vue';
 import ProfilePage from './views/ProfilePage.vue';
+import './styles/global.css';
 
 Vue.use(VueRouter);
 
