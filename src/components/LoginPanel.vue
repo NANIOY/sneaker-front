cc013f07bb78c408b48204c9723dc20525c53527
Nvnchi <r0879363@student.thomasmore.be>
2023-12-15 13:45:16 +0100

    feat: create log in panel

--- src/components/LoginPanel.vue
+++ src/components/LoginPanel.vue
@@ -1,10 +1,32 @@
-<script setup>
-
-</script>
-
 <template>
-  LOGIN PAGE
+  <div class="login-panel">
+    <form @submit.prevent="login">
+      <input type="text" v-model="username" placeholder="Username" />
+      <input type="password" v-model="password" placeholder="Password" />
+      <button type="submit">Log in</button>
+    </form>
+  </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      this.$emit('login', {
+        username: this.username,
+        password: this.password
+      });
+    }
+  }
+};
+</script>
+
+<style>
+/* Your CSS styles for the login panel */
 </style>
