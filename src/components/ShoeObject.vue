8a5914b6b630b842af7ab5c5b132fb881f8804a7
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-15 20:14:22 +0100

    style: change block size, margin, add status styling

--- src/components/ShoeObject.vue
+++ src/components/ShoeObject.vue
@@ -2,12 +2,14 @@
   <div class="shoe-object__block" :style="{ borderColor: color }">
     <div class="shoe-details">
       <h1 class="header header__small">{{ shoeType }}</h1>
-      <p class="text text__normal text__color--black">{{ userName }}</p>
-      <p class="text text__normal text__color--black">{{ userEmail }}</p>
-      <p class="text text__small text__color--black">{{ userId }}</p>
+      <div class="shoe-object__user">
+        <p class="text text__normal text__color--black">{{ userName }}</p>
+        <p class="text text__normal text__color--black">{{ userEmail }}</p>
+      </div>
+      <p class="text text--userid text__small text__color--black">{{ userId }}</p>
     </div>
-    <div class="shoe-status" :style="{ backgroundColor: color }">
-      <span>{{ status }}</span>
+    <div class="shoe-status surface__dark" :style="{ backgroundColor: color }">
+      <span class="text text__normal text__color--white">{{ status }}</span>
     </div>
   </div>
 </template>
@@ -21,12 +23,13 @@ export default {
 <style scoped>
 .shoe-object__block {
   width: 384px;
-  height: 120px;
+  height: 240px;
   border: 1px solid;
   display: flex;
   justify-content: space-between;
   align-items: end;
   padding: 12px;
+  position: relative;
 }
 
 .shoe-details {
@@ -35,17 +38,27 @@ export default {
 
 h1 {
   color: var(--black);
-}
-
-.text {
   margin-bottom: 8px;
 }
 
+.shoe-object__user p {
+  margin: 2px 0;
+}
+
 .shoe-status {
-  width: 100px;
-  height: 100%;
+  width: auto;
+  height: 24px;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  padding: 8px;
+  border-radius: 64px;
+}
+
+.text--userid {
+  margin-bottom: 8px;
 }
 </style>
