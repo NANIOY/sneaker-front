41114788057051cdcdc20c1142e1b4d956612fa5
Nvnchi <r0879363@student.thomasmore.be>
2023-12-15 18:26:58 +0100

    feat: add sorting system

--- src/components/SortingButton.vue
+++ src/components/SortingButton.vue
@@ -1,10 +1,25 @@
-<script setup>
-
-</script>
-
 <template>
-  SORTING BUTTON
+  <button @click="toggleSortOrder">
+    Sort Order: {{ sortOrder }}
+  </button>
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      sortOrder: 'desc'
+    };
+  },
+  methods: {
+    toggleSortOrder() {
+      this.sortOrder = this.sortOrder === 'desc' ? 'asc' : 'desc';
+      this.$emit('sort-changed', this.sortOrder);
+    }
+  }
+};
+</script>
+
+<style>
+/* Your CSS styles for SortingButton */
 </style>
