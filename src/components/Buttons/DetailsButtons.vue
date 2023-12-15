9b84f780f7cfd74d8cbb45912fb3ba63eddf8832
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-15 20:26:04 +0100

    feat: create button for details

---
+++ src/components/Buttons/DetailsButtons.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="details-button">
+      <span class="button-text">Details</span>
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DetailsButton',
+  };
+  </script>
+  
+  <style scoped>
+  .details-button {
+    width: 100%;
+    height: 28px;
+    background-color: var(--black);
+    color: var(--white);
+    border: none;
+    cursor: pointer;
+  }
+  
+  .button-text {
+    font-size: 14px;
+  }
+  </style>
+  
