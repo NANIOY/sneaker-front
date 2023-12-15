b4b869c9c41b205d9f546e83716afa2c4040990c
Nvnchi <r0879363@student.thomasmore.be>
2023-12-15 13:47:05 +0100

    feat: create login page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,10 +1,42 @@
-<script setup>
-
-</script>
-
 <template>
-  LOGINPAGE
+  <div class="login-page">
+    <logo-component></logo-component>
+    <login-panel @login="handleLogin"></login-panel>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import LoginPanel from '../components/LoginPanel.vue';
+import LogoComponent from '../components/LogoComponent.vue';
+
+export default {
+  components: {
+    LoginPanel,
+    LogoComponent
+  },
+  methods: {
+    handleLogin(credentials) {
+      fetch('http://example.com/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials)
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Login Successful:', data);
+        // Handle login success, e.g., storing the auth token and redirecting
+      })
+      .catch(error => {
+        console.error('Login Error:', error);
+        // Handle login error, e.g., displaying a message to the user
+      });
+    }
+  }
+};
+</script>
+
+<style>
+/* Your CSS styles */
 </style>
