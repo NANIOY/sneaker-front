dbd9170a03d0fae6d3b1aea02379c6bf9414fa80
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-15 16:40:01 +0100

    feat: create basic navbutton

---
+++ src/components/Buttons/NavButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <div @click="handleClick" :class="{ active: active }">
+      <img :src="iconSrc" alt="Icon" class="icon" />
+      <span class="button button__large">{{ label }}</span>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      label: String,
+      iconSrc: String,
+      active: Boolean,
+    },
+    methods: {
+      handleClick() {
+        console.log(`${this.label} Button Clicked!`);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  div {
+    cursor: pointer;
+    max-width: 144px;
+    height: 48px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--green);
+  }
+  
+  .icon {
+    width: 24px;
+    height: 24px;
+    margin-right: 8px;
+  }
+  
+  /* Media query for responsiveness */
+  @media (max-width: 144px) {
+    div {
+      max-width: 100%;
+    }
+  }
+  </style>
+  
