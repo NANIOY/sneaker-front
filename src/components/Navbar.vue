0126721da68115193d82e68c97032ddbbf46b68b
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-15 16:40:27 +0100

    feat: import navbuttons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,26 @@
-<script setup>
-
-</script>
-
 <template>
-  NAVBAR COMPONENT
+  <div class="navbar">
+    <NavButton label="Orders" iconSrc="../../assets/icons/orders-active.webp" :active="isOrdersActive" />
+    <NavButton label="Profile" iconSrc="../assets/icons/profile.webp" :active="isProfileActive" />
+  </div>
 </template>
+
+<script>
+import NavButton from '../components/Buttons/NavButton.vue';
+
+
+export default {
+  components: {
+    NavButton,
+  },
+  data() {
+    return {
+      isOrdersActive: true,
+      isProfileActive: false,
+    };
+  },
+};
+</script>
 
 <style scoped>
 </style>
