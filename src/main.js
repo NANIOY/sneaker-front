85bc6783b60c823d42c98db79c9012f4d919c23e
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-15 17:47:39 +0100

    fix: import normalize first

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
+import './styles/normalize.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './styles/global.css';
-import './styles/normalize.css';
-
 const app = createApp(App);
 
 app.use(router); // Use the router with the app
