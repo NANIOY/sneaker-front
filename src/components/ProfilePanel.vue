a3202544f4695c40e1aac806d85c58fec7380841
Sidney <r0879363@student.thomasmore.be>
2023-12-16 23:24:19 +0100

    refractor & style: rename classes and add styling and icon

--- src/components/ProfilePanel.vue
+++ src/components/ProfilePanel.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="profile-container__block gradient-background">
-    <h1 class="login-panel__block__title header header__large">Profile</h1>
+    <h1 class=" header header__large">Profile</h1>
     <form class="text" @submit.prevent="submitProfile">
       <div class="input-group">
-        <label class="login-panel__block__label text__large label" for="username">Username</label>
-        <input class="login-panel__block__input input__text input" id="username" type="text" v-model="user.username" placeholder="Username"/>
+        <label class="text__large label" for="username">Username</label>
+        <input class="profile-panel__block__input input__text input" id="username" type="text" v-model="user.username" placeholder="Username"/>
+        <span><img class="profile-panel__block__input__icon" src="../assets/icons/pencil.webp" alt="icon of a pencil"></span>
       </div>
       <div class="input-group">
-        <label class="login-panel__block__label text__large label" for="email">Email</label>
-        <input class="login-panel__block__input input__text input" id="email" type="email" v-model="user.email" placeholder="Email"/>
+        <label class="text__large label" for="email">Email</label>
+        <input class="profile-panel__block__input input__text input" id="email" type="email" v-model="user.email" placeholder="Email"/>
+        <span><img class="profile-panel__block__input__icon" src="../assets/icons/pencil.webp" alt="icon of a pencil"></span>
       </div>
       <div class="input-group">
-        <label class="login-panel__block__label text__large label" for="new-password">New Password</label>
-        <input class="login-panel__block__input input__text input" id="new-password" type="password" v-model="passwords.newPassword" placeholder="New password"/>
+        <label class="text__large label" for="new-password">New Password</label>
+        <input class="profile-panel__block__input input__text input" id="new-password" type="password" v-model="passwords.newPassword" placeholder="New password"/>
+        <span><img class="profile-panel__block__input__icon" src="../assets/icons/pencil.webp" alt="icon of a pencil"></span>
       </div>
       <Button class="login-panel__block__button" @click="submitProfile">Update Profile</Button>
     </form>
@@ -74,8 +77,21 @@ export default {
   margin-bottom: 8px;
 }
 
-.login-panel__block__input {
+.profile-panel__block__input {
   border-width: 0px 0px 1px 0px !important;
+  padding-right: 28px;
+  padding-left: 4px;
+  border-color: var(--white); 
+  outline: none; 
 }
-/* Add more styles as per your design requirements */
+
+.profile-panel__block__input:focus {
+  border-color: var(--white); 
+}
+
+.profile-panel__block__input__icon {
+  width: 16px;
+  height: 16px;
+  margin-left: -16px;
+}
 </style>
