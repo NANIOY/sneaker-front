877ab7c16ea7b5f3259a17be7f633b8244fc2102
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-16 14:39:34 +0100

    feat: add ws logic

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './styles/global.css';
+
+const socket = new WebSocket('wss://sneaker-back.onrender.com/primus');
+
+socket.addEventListener('open', (event) => {
+  console.log('WebSocket connection opened:', event);
+});
+
+socket.addEventListener('message', (event) => {
+  console.log('WebSocket message received:', event);
+  const data = JSON.parse(event.data);
+});
+
 const app = createApp(App);
-
-app.use(router); // Use the router with the app
-
+app.config.globalProperties.$socket = socket;
+app.use(router);
 app.mount('#app');
 
