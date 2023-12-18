f5d7acbb2e97c0560683128218341739b495f768
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-18 21:48:35 +0100

    chore: remove duplicate SortingButton, move to Buttons folder

--- src/components/Buttons/SortingButton.vue
+++ src/components/Buttons/SortingButton.vue
@@ -1,24 +1,25 @@
 <template>
-    <button @click="toggleSortOrder">
-      Sort Order: {{ sortOrder }}
-    </button>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      sortOrder: String
-    },
-    methods: {
-      toggleSortOrder() {
-        this.$emit('sort-changed', this.sortOrder === 'asc' ? 'desc' : 'asc');
-      }
+  <div class="button button__normal text__color--black" @click="toggleSortOrder">
+    <span> Sort Order: {{ sortOrder }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sortOrder: String
+  },
+  methods: {
+    toggleSortOrder() {
+      this.$emit('sort-changed', this.sortOrder === 'asc' ? 'desc' : 'asc');
     }
-  };
-  </script>
-  
-  <style scoped>
-  /* button {
-  } */
-  </style>
-  
+  }
+};
+</script>
+
+<style scoped>
+div {
+  cursor: pointer;
+  user-select: none;
+}
+</style>
